1ed57e401d49907d8739316ce2e13027fdfe5510
Felipe Magrassi <felipe.1magrassi@gmail.com>
2023-09-13 21:42:58 -0300

    types

--- 2/main.go
+++ 2/main.go
@@ -2,12 +2,15 @@ package main
 
 const t = true
 
+type ID int
+
 var (
 	a bool    = true
 	b bool    = false
 	c string  = "hello"
 	d int     = 1
 	e float64 = 1.2
+	g ID      = 1
 )
 
 func main() {
@@ -15,5 +18,6 @@ func main() {
 	f := true
 
 	// f := false // error: no new variables on left side of :=
-	println(a, b, c, d, e, f, t)
+	println(a, b, c, d, e, f, g, t)
+
 }
