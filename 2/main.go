1fe75c15c7338d0dc615ed5f8d726e6e7e7bb733
Felipe Magrassi <felipe.1magrassi@gmail.com>
2023-09-13 21:49:59 -0300

    type check

--- 2/main.go
+++ 2/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const t = true
 
 type ID int
@@ -19,5 +21,8 @@ func main() {
 
 	// f := false // error: no new variables on left side of :=
 	println(a, b, c, d, e, f, g, t)
+
+	fmt.Printf("a: %T\n", a)
+	fmt.Printf("g: %T\n", g)
 
 }
