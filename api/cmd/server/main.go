e22a958b80af76bb8a4cd5c0448a86ccad39c72b
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2023-12-24 17:06:12 -0300

    add swagger

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
@@ -8,12 +9,37 @@ import (
 	"github.com/go-chi/jwtauth"
 
 	"github.com/felipemagrassi/go-expert/api/configs"
+	_ "github.com/felipemagrassi/go-expert/api/docs"
 	"github.com/felipemagrassi/go-expert/api/internal/entity"
 	"github.com/felipemagrassi/go-expert/api/internal/infra/database"
 	"github.com/felipemagrassi/go-expert/api/internal/infra/handlers"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8081
+// @BasePath  /
+
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 
 func main() {
 	configs, err := configs.LoadConfig(".")
@@ -37,8 +63,10 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Route("/products", func(r chi.Router) {
+		// r.Use(LogRequest)
 		r.Use(jwtauth.Verifier(configs.TokenAuth))
 		r.Use(jwtauth.Authenticator)
 		r.Post("/", productHandler.CreateProduct)
@@ -53,5 +81,14 @@ func main() {
 		r.Post("/auth", userHandler.GetJWT)
 	})
 
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8081/swagger/doc.json")))
+
 	http.ListenAndServe(":8081", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
