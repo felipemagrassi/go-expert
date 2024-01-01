663202770f1768565da5db25e85aab8b1377e180
Felipe Magrassi <Felipe.1magrassi@gmail.com>
2024-01-01 12:44:02 -0300

    category find and create

---
+++ graphql/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/felipemagrassi/go-expert/graphql/graph"
+	"github.com/felipemagrassi/go-expert/graphql/internal/database"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatalf("can't open database: %v", err)
+	}
+
+	defer db.Close()
+
+	if port == "" {
+		port = defaultPort
+	}
+
+	categoryDb := database.NewCategory(db)
+	resolver := &graph.Resolver{
+		CategoryDB: categoryDb,
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: resolver}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
